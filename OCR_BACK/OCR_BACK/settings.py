de846382380053ad0cfc9c6f0e7bea328dbd04f8
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-29 19:50:49 -0600

    fixes

--- OCR_BACK/OCR_BACK/settings.py
+++ OCR_BACK/OCR_BACK/settings.py
@@ -32,7 +32,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['ocr-reto.onrender.com']
 
-CORS_ALLOWED_ORIGINS = [os.getenv("CORS_ALLOWED_ORIGINS1")]
+CORS_ALLOWED_ORIGINS = ['https://ocr-reto-front.onrender.com']
 
 # Application definition
 
