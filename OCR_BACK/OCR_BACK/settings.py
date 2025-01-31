6d6be3e8af92d4865c1899f948cec490b0a2423a
SeguRuiz <ruizsegura96199@gmail.com>
2025-01-30 22:33:09 -0600

    version: 1.0.0

--- OCR_BACK/OCR_BACK/settings.py
+++ OCR_BACK/OCR_BACK/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO-SECRET-KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["ocr-reto.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
